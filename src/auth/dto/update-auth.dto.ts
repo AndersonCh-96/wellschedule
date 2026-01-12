7f3356ed27a2882225be429dd563cd0cff99ca0f
AndersonCh-96 <0401995154a@gmail.com>
2026-01-12 09:56:57 -0500

    add validation

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -6,7 +6,9 @@ import {
   IsBoolean,
   IsNotEmpty,
   IsOptional,
+  IsString,
   IsUUID,
+  MinLength,
 } from "class-validator";
 import { Role } from "src/roles/entities/role.entity";
 
@@ -15,6 +17,12 @@ export class UpdateAuthDto extends PartialType(CreateAuthDto) {
   @IsArray()
   @ArrayMinSize(1, { message: "Debe tener al menos un rol" })
   roles?: Role[];
+
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6, { message: "La contraseña debe tener al menos 6 caracteres" })
+  password!: string;
 
   @IsOptional()
   @IsBoolean()
